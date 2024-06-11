d6ebfe6a8f31aa437d4e3cd54d5d02f62634d615
Jan Guz <1janekguz1@gmail.com>
2024-06-11 17:45:37 +0200

    variable names changed

--- client/src/guess_by_album_cover/GuessByAlbumCover.tsx
+++ client/src/guess_by_album_cover/GuessByAlbumCover.tsx
@@ -1,59 +1,55 @@
 import React, { useState } from "react";
-//import inBlur from "../../public/by_Blur_test.jpg";
 import inRainbow from "../../public/by_Inrainbowscover_test.png";
 
 const GuessByAlbumCover: React.FC = () => {
-  const [blurredPanels, setBlurredPanels] = useState<number[]>([]);
+  const [visiblePanels, setVisiblePanels] = useState<number[]>([]);
 
-  const toggleBlur = () => {
-    const newBlurredPanels = [...blurredPanels];
-
+  const removeBlur = () => {
     const availableIndexes = Array.from(
       { length: 9 },
       (_, index) => index
-    ).filter((index) => !newBlurredPanels.includes(index));
+    ).filter((index) => !visiblePanels.includes(index));
 
     if (availableIndexes.length === 0) {
-      setBlurredPanels([]);
+      setVisiblePanels([]);
       return;
     }
 
     const randomIndex =
       availableIndexes[Math.floor(Math.random() * availableIndexes.length)];
 
-    newBlurredPanels.push(randomIndex);
-    setBlurredPanels(newBlurredPanels);
+    setVisiblePanels([...visiblePanels, randomIndex]);
   };
 
   return (
     <div className="h-screen flex flex-col items-center bg-gray-900 poppins-semibold">
-      <h2 className="text-5xl text-center text-green-500 align-top mt-4 mb-5">
+      <h2 className="text-5xl text-center text-green-500 mt-4 mb-5">
         Guess By Album Cover
       </h2>
       <div className="relative flex items-center h-full">
         <img
-          className="mx-auto border rounded p-2 justify-center w-80 h-80"
+          className="mx-auto border rounded p-2 w-80 h-80"
           src={inRainbow}
-          alt="Blur Test"
-        ></img>
-        <div className="grid absolute grid-cols-3 grid-rows-3 w-80 h-80">
-          {Array.from({ length: 9 }).map((_, index) => (
+          alt="Album Cover"
+        />
+        <div className="absolute grid grid-cols-3 grid-rows-3 w-80 h-80">
+          {Array.from({ length: 9 }, (_, index) => (
             <div
               key={index}
-              className={`bg-transparent w-full h-full ${
-                blurredPanels.includes(index)
-                  ? ""
+              className={`w-full h-full ${
+                visiblePanels.includes(index)
+                  ? "bg-transparent"
                   : "backdrop-filter backdrop-blur-md"
               }`}
-            ></div>
+            />
           ))}
         </div>
       </div>
       <button
-        onClick={toggleBlur}
+        onClick={removeBlur}
         className="mt-4 p-2 bg-blue-500 text-white rounded-md"
       >
-        Toggle Blur
+        Remove Blur
       </button>
     </div>
   );
