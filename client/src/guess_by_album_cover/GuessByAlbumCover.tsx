aaadfb0e5d2f92bd57182cc0222897e4d1ce224f
kadray <1janekguz1@gmail.com>
2024-06-12 22:08:32 +0200

    wow

--- client/src/guess_by_album_cover/GuessByAlbumCover.tsx
+++ client/src/guess_by_album_cover/GuessByAlbumCover.tsx
@@ -1,60 +1,86 @@
 import React, { useState } from "react";
-//import inBlur from "../../public/by_Blur_test.jpg";
+import { FaHeart } from "react-icons/fa"; // Importuj ikonę serca
 import inRainbow from "../../public/by_Inrainbowscover_test.png";
 
 const GuessByAlbumCover: React.FC = () => {
-  const [blurredPanels, setBlurredPanels] = useState<number[]>([]);
+  const [visiblePanels, setVisiblePanels] = useState<number[]>([]);
+  const [inputValue, setInputValue] = useState("");
+  const albumName = "123";
 
-  const toggleBlur = () => {
-    const newBlurredPanels = [...blurredPanels];
-
+  const removeBlur = () => {
     const availableIndexes = Array.from(
       { length: 9 },
       (_, index) => index
-    ).filter((index) => !newBlurredPanels.includes(index));
+    ).filter((index) => !visiblePanels.includes(index));
 
     if (availableIndexes.length === 0) {
-      setBlurredPanels([]);
+      setVisiblePanels([]);
       return;
     }
 
     const randomIndex =
       availableIndexes[Math.floor(Math.random() * availableIndexes.length)];
 
-    newBlurredPanels.push(randomIndex);
-    setBlurredPanels(newBlurredPanels);
+    setVisiblePanels([...visiblePanels, randomIndex]);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleChange = () => {
+    if (inputValue !== albumName) {
+      removeBlur();
+    } else {
+      console.log("nice");
+    }
   };
 
   return (
-    <div className="h-screen flex flex-col items-center bg-gray-900 poppins-semibold">
-      <h2 className="text-5xl text-center text-green-500 align-top mt-4 mb-5">
+    <div className="h-screen flex flex-col items-center bg-gray1 poppins-semibold">
+      <h2 className="text-5xl text-center text-green-500 mt-4 mb-5">
         Guess By Album Cover
       </h2>
-      <div className="relative flex items-center h-full">
+      <div className="mt-48 relative flex items-center">
         <img
-          className="mx-auto border rounded p-2 justify-center w-80 h-80"
+          className="mx-auto border rounded p-2 w-80 h-80"
           src={inRainbow}
-          alt="Blur Test"
-        ></img>
-        <div className="grid absolute grid-cols-3 grid-rows-3 w-80 h-80">
-          {Array.from({ length: 9 }).map((_, index) => (
+          alt="Album Cover"
+        />
+        <div className="absolute grid grid-cols-3 grid-rows-3 w-80 h-80">
+          {Array.from({ length: 9 }, (_, index) => (
             <div
               key={index}
-              className={`bg-transparent w-full h-full ${
-                blurredPanels.includes(index)
-                  ? ""
+              className={`w-full h-full ${
+                visiblePanels.includes(index)
+                  ? "bg-transparent"
                   : "backdrop-filter backdrop-blur-md"
               }`}
-            ></div>
+            />
           ))}
         </div>
       </div>
-      <button
-        onClick={toggleBlur}
-        className="mt-4 p-2 bg-blue-500 text-white rounded-md"
-      >
-        Toggle Blur
-      </button>
+      <div className="flex mt-4">
+        <input
+          className="p-2 rounded-lg mr-1"
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button
+          onClick={handleChange}
+          className="p-2 bg-blue-500 hover:bg-slate-600 text-white rounded-lg flex items-center"
+        >
+          Submit
+        </button>
+      </div>
+      <div className="mt-4 flex">
+        <FaHeart className="size-10 text-red-600 mr-1" />
+        <FaHeart className="size-10 text-red-600 mr-1" />
+        <FaHeart className="size-10 text-red-600 mr-1" />
+        <FaHeart className="size-10 text-red-600 mr-1" />
+        <FaHeart className="size-10 text-red-600 mr-1" />
+      </div>
     </div>
   );
 };
