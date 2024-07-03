e63ba5e591e71e1c7c1b1049a00b7925ccafebf4
Maciej Klimek <87934529+maciej-klimek@users.noreply.github.com>
2024-07-03 15:46:23 +0200

    cleanup after merge

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const AWS = require('aws-sdk');
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION // or your preferred region
+    region: process.env.AWS_REGION
 });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
