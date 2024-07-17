13c76f995ed347e333da0da459dc9c26cf19acc8
Maciej Klimek <87934529+maciej-klimek@users.noreply.github.com>
2024-07-17 16:28:08 +0200

    test

--- server/auth_server.js
+++ server/auth_server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router();
 const SpotifyWebApi = require('spotify-web-api-node');
 
+console.log("AAAAAAAAAAAAAAAAA")
+
 // Trasa logowania do Spotify
 router.post("/login", (req, res) => {
     const code = req.body.code;
