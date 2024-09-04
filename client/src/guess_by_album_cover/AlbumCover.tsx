2d2e8c7a6e8d646fa444850a09de5a2cf0edf334
Maciej Klimek <87934529+maciej-klimek@users.noreply.github.com>
2024-09-04 12:56:47 +0200

    ui tweak in gbac

--- client/src/guess_by_album_cover/AlbumCover.tsx
+++ client/src/guess_by_album_cover/AlbumCover.tsx
@@ -7,19 +7,21 @@ interface AlbumCoverProps {
 
 const AlbumCover: React.FC<AlbumCoverProps> = ({ imageUrl, visiblePanels }) => {
   return (
-    <div className="relative flex items-center">
-      <img className="mx-auto p-2 w-80 h-80" src={imageUrl} alt="Album Cover" />
-      <div className="absolute grid grid-cols-3 grid-rows-3 w-80 h-80">
-        {Array.from({ length: 9 }, (_, index) => (
-          <div
-            key={index}
-            className={`w-full h-full ${
-              visiblePanels.includes(index)
-                ? "bg-transparent"
-                : "backdrop-filter backdrop-blur-md"
-            }`}
-          />
-        ))}
+    <div className="flex justify-center items-center">
+      <div className="relative flex items-center justify-center">
+        <img className="w-80 h-80" src={imageUrl} alt="Album Cover" />
+        <div className="absolute grid grid-cols-3 grid-rows-3 w-80 h-80">
+          {Array.from({ length: 9 }, (_, index) => (
+            <div
+              key={index}
+              className={`w-full h-full ${
+                visiblePanels.includes(index)
+                  ? "bg-transparent"
+                  : "backdrop-filter backdrop-blur-md"
+              }`}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
