479a1bfcec886bbef398b9c4d166f28a3018c407
Maciej Klimek <87934529+maciej-klimek@users.noreply.github.com>
2024-09-08 20:13:38 +0200

    no user icon fix

--- client/src/Settings.tsx
+++ client/src/Settings.tsx
@@ -11,41 +11,27 @@ const Settings: React.FC<SettingsProps> = ({ userImage, userScore }) => {
   const toggleSettingsMenu = () => {
     setIsSettingsMenuOpen(!isSettingsMenuOpen);
   };
+
   return (
     <div className="absolute top-8 right-8 transition-transform duration-300 ease-in-out transform hover:scale-105">
       <button onClick={toggleSettingsMenu}>
-        {userImage && (
-          <div className="flex items-center">
-            <div className="pr-6">
-              <div className="text-sm text-neutral-700">You currently have</div>
-              <div className="text-xl">{userScore} points!</div>
-            </div>
-            <img
-              src={userImage}
-              alt="User"
-              className="w-12 h-12 rounded-full"
-            />
+        <div className="flex items-center">
+          <div className="pr-6">
+            <div className="text-sm text-neutral-700">You currently have</div>
+            <div className="text-xl">{userScore} points!</div>
           </div>
-        )}
+          <img src={userImage || "user_icon.png"} alt="User" className="w-12 h-12 rounded-full" />
+        </div>
       </button>
       {isSettingsMenuOpen && (
-        <div className=" text-neutral-600 absolute top-10 right-0 mt-8 w-52 bg-gray2 rounded-lg shadow-lg py-2">
-          <Link
-            to="/your-top-songs"
-            className="block px-4 py-2 hover:bg-neutral-800 hover:text-green-500"
-          >
+        <div className="text-neutral-600 absolute top-10 right-0 mt-8 w-52 bg-gray2 rounded-lg shadow-lg py-2">
+          <Link to="/your-top-songs" className="block px-4 py-2 hover:bg-neutral-800 hover:text-green-500">
             Your Top Songs
           </Link>
-          <Link
-            to="/ranking"
-            className="block px-4 py-2 hover:bg-neutral-800 hover:text-green-500"
-          >
+          <Link to="/ranking" className="block px-4 py-2 hover:bg-neutral-800 hover:text-green-500">
             Ranking
           </Link>
-          <Link
-            to="/logout"
-            className="block px-4 py-2 hover:bg-neutral-800 hover:text-green-500"
-          >
+          <Link to="/logout" className="block px-4 py-2 hover:bg-neutral-800 hover:text-green-500">
             Log Out
           </Link>
         </div>
